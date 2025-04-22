5973e33ae7eaa7ff24d7f207003c3b137d31cac9
Mulwahz <sean.mutuku@strathmore.edu>
2025-04-22 09:51:28 +0300

    changed path from 1.dnsforward to Special Topics

--- Special Topics/dnsForward.bash
+++ Special Topics/dnsForward.bash
@@ -8,16 +8,16 @@ apt install bind9 bind9utils -y
 # cd /etc/bind
 #nano /etc/bind/named.conf.options
 #Copy template file
-cp /home/netadmin/1.dnsForward/templates/etc/bind/named.conf.options /etc/bind/named.conf.options
+cp /home/netadmin/Special Topics/templates/etc/bind/named.conf.options /etc/bind/named.conf.options
 
 # 3. Add a zone for the domain
 #nano /etc/bind/named.conf.default-zones
-cp /home/netadmin/1.dnsForward/templates/etc/bind/named.conf.default-zones /etc/bind/named.conf.default-zones
+cp /home/netadmin/Special Topics/templates/etc/bind/named.conf.default-zones /etc/bind/named.conf.default-zones
 
 # 4. Create the zone file
 #cp /etc/bind/db.local /etc/bind/db.mulwa.cns
 #nano /etc/bind/db.mulwa.cns
-cp /home/netadmin/1.dnsForward/templates/etc/bind/db.mulwa.cns /etc/bind/db.mulwa.cns
+cp /home/netadmin/Special Topics/templates/etc/bind/db.mulwa.cns /etc/bind/db.mulwa.cns
 
 # 5. Verify Setup
 named-checkconf
